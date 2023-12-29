481c7c14ad4e608a41c6d0bf7de64bd04db08ae3
kamaraj <kamaraj.mariyappan@veris.in>
2023-12-29 13:07:52 +0530

    initaial commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
-import { StyleSheet, View, Text } from "react-native";
-import { multiply, Button } from "veris-react-native";
+import * as React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { multiply, Button } from 'veris-react-native';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
